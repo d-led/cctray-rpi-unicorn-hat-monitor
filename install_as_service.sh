c63f2180f4d9dfa72c7713059ec381c46c7ef6fb
Dmitry Ledentsov <dmlled@yahoo.com>
2019-01-05 12:47:24 +0100

    args [ci skip]

--- install_as_service.sh
+++ install_as_service.sh
@@ -27,7 +27,7 @@ After=multi-user.target
 
 [Service]
 Type=idle
-ExecStart=/usr/bin/python ${origin}/poll_hd.py ${urls} > $origin/cctray.log 2>&1
+ExecStart=/usr/bin/python ${wait_s} ${origin}/poll_hd.py ${urls} > $origin/cctray.log 2>&1
 
 [Install]
 WantedBy=multi-user.target
